1e537bef0d06cc44dd1d207cfdafcfa555af48a7
JavierAlmonacid-10 <javialmonacidtorres@gmail.com>
2025-10-23 20:25:09 -0500

    Creación del web zoologico con Angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { AnimalComponent } from './components/animal-component/animal-component';
+
+export const routes: Routes = [
+
+{
+path:"animal",component:AnimalComponent
+}
+
+];
